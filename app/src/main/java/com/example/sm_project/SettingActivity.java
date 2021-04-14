e0d8bbe4aa573428cf079b4d5bbc7931f193a846
H-Subin <hsubin32@gmail.com>
2021-04-15 01:37:50 +0900

    .

--- app/src/main/java/com/example/sm_project/SettingActivity.java
+++ app/src/main/java/com/example/sm_project/SettingActivity.java
@@ -8,13 +8,14 @@ import android.view.View;
 import android.widget.Button;
 
 public class SettingActivity extends AppCompatActivity {
+    Button setting_cancle_btn, setting_changre_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
 
-        Button setting_cancle_btn, setting_changre_btn;
+
         setting_cancle_btn = findViewById(R.id.setting_cancel_btn);
         setting_changre_btn = findViewById(R.id.setting_change_btn);
 
